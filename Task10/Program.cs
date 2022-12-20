6a123037ad45a9637af3a3343dd2432cbaf562f2
Sultan <sultan@gmail.com>
2022-12-20 19:54:34 +0300

    add dz2 and gitignore

---
+++ Task10/Program.cs
@@ -0,0 +1,3 @@
+﻿int number = Convert.
+ToInt32(Console.ReadLine());
+System.Console.WriteLine(number / 10 % 10);
---
+++ Task15/Program.cs
@@ -0,0 +1,10 @@
+﻿int num = Convert.
+ToInt32(Console.ReadLine());
+if (num == 6 || num == 7)
+{
+    System.Console.WriteLine("да");
+}
+else
+{
+    System.Console.WriteLine("нет");
+}

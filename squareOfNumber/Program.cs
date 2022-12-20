00c900cffe1278851715d34ebd8098869cd39b78
Sultan <sultan@gmail.com>
2022-12-20 11:09:35 +0300

    add squareOfNumber

---
+++ squareOfNumber/Program.cs
@@ -0,0 +1,20 @@
+﻿// System.Console.WriteLine("Введите число");
+// //Readline преобразовывает в string
+// int number = Convert.ToInt32(Console.ReadLine());
+// System.Console.WriteLine(
+//     $"Square of number is {number * number}");
+// //конкатенация
+// System.Console.WriteLine(
+//     "Square of number is " + number * number);
+//Вариант Ильи
+System.Console.WriteLine("Введите число");
+string stringNumber = Console.ReadLine()!.Trim();
+int number;
+while (!int.TryParse(stringNumber, out number))
+{
+    System.Console.WriteLine("Введите число,не строку");
+    //Trim убирает пробелы с конца и сначала
+    stringNumber = Console.ReadLine()!.Trim();
+}
+System.Console.WriteLine(
+    $"Square of number is {number * number}");

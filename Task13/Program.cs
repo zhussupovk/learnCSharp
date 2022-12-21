9493937a2e1c7ceb732e9913bc9625166ff04032
Sultan <sultan@gmail.com>
2022-12-21 12:27:58 +0300

    add Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,26 @@
+﻿int number = Convert.
+ToInt32(Console.ReadLine());
+int ThirdDigit(int number)
+{
+    int result = -1;
+    if (number >= 100)
+    {
+        while (number > 999)
+        {
+            number /= 10;
+        }
+        result = number % 10;
+    }
+    return result;
+}
+if (number < 100)
+{
+    System.
+    Console.WriteLine("третьей цифры нет");
+}
+else
+{
+    System.
+    Console.
+    WriteLine(ThirdDigit(number));
+}

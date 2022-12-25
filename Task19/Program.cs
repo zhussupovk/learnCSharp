e833ac8783f7cd826cc9651ea18dd09555b874ab
Sultan <sultan@gmail.com>
2022-12-25 15:20:08 +0300

    changed task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,10 +1,11 @@
-﻿int number = Convert.ToInt32(Console.ReadLine());
+﻿// int number = Convert.ToInt32(Console.ReadLine());
+int number =12821;
 int a = number, b = 0, temple;
 while (number != 0)
 {
     temple = number % 10;
     number = number / 10;
-    b = b % 10 + temple;
+    b = b * 10 + temple;
 }
 if (a == b)
 {
@@ -12,5 +13,5 @@ if (a == b)
 }
 else
 {
-    System.Console.WriteLine("Yes");
+    System.Console.WriteLine("No");
 }

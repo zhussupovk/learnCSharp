02ab85938c51a207a0087a08903aa554fe2ececd
Sultan <sultan@gmail.com>
2023-01-16 16:50:31 +0300

    task25

---
+++ task25/Program.cs
@@ -0,0 +1,8 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int temp=a;
+for (int i = 0; i < b-1; i++)
+{
+    a=a*temp;
+}
+System.Console.WriteLine(a);

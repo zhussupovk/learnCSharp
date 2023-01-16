07cd9bc49a73f02fcd7da4c8e8253782bb6b81f3
Sultan <sultan@gmail.com>
2023-01-16 17:46:30 +0300

    dz

---
+++ task27/Program.cs
@@ -0,0 +1,3 @@
+﻿int number = Convert.ToInt32(Console.ReadLine());
+int res=number%10+number/100+number%100/10;
+System.Console.WriteLine(res);
---
+++ task29/Program.cs
@@ -0,0 +1,6 @@
+﻿int[] arr=new int[8] ;
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i]=new Random().Next(0,101);
+    System.Console.WriteLine(arr[i]);
+}
